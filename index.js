8427a24d6d4b96e6f222348770f64d0d5188112c
fortuinp <pinda.fortuin@gmail.com>
2024-02-16 14:17:57 +0200

    API

---
+++ index.js
@@ -0,0 +1,40 @@
+import { express, userRouter } from "./controllers/UserController.js";
+import { productRouter } from "./controllers/ProductController.js";
+import cookieParser from "cookie-parser";
+import { errorHandling } from "./middleware/ErrorHandling.js";
+import path from 'path'
+import { config } from "dotenv";
+config()
+
+const app=express()
+const port = +process.env.PORT || 4000
+
+//Middleware
+app.use((req,res,next)=>{
+res.header("Access-Control-Allow-Origin","*")
+res.header("Access-Control-Allow-Credentials","*")
+res.header("Access-Control-Allow-Methods","*")
+res.header("Access-Control-Request-Methods","*")
+res.header("Access-Control-Allow-Headers","*")
+res.header("Access-Control-Expose-Headers","*");
+next();
+}) 
+
+app.use(
+    express.static('./static'),
+    express.json(),
+    express.urlencoded({
+        extended:true,
+    }),
+    cookieParser(),
+    cors()
+)
+app.get('^/$|/lifechoices',(req,res)=>{
+res.status(200).sendFile(path.join(__dirname,'./static/index.html'))
+})
+app.use('./users',userRouter)
+app.use('/products',productRouter)
+app.use(errorHandling)
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
+})
--- model/index.js
+++ model/index.js
@@ -1,5 +1,5 @@
-import { Users } from "./Users";
-import { Products } from "./Products";
+import { Users } from "./Users.js";
+import { Products } from "./Products.js";
 //create objects
 let users=new Users()
 let products=new Products()
